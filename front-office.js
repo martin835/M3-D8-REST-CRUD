4aea385a03f575b5f7890c0375a7ec69e732166f
martin835 <64438132+martin835@users.noreply.github.com>
2022-02-02 14:47:47 +0100

    cards instead of a list

--- front-office.js
+++ front-office.js
@@ -4,8 +4,6 @@
 
 /* ASYNC FUNCTIONS */
 const getData = async  () => {
-  
-
   const URL = "https://striveschool-api.herokuapp.com/api/product/"
   
     try {
@@ -19,21 +17,43 @@ const getData = async  () => {
         const data = await response.json()
 
         console.log(data)
+        loadProducts(data)
 
     } catch (error) {
-    
-}
-
+    console.log(error)
+    }
     
 };
-
-
 
 
 
 /* ASYNC FUNCTIONS END */
 
 /* OTHER FUNCTIONS */
+
+const loadProducts = (arr) => {
+    const productsContainer = document.getElementById("productsContainer");
+    productsContainer.innerHTML = "";
+    arr.forEach(element => {
+        let newCol = document.createElement("div")
+        newCol.classList.add("col")
+        
+        newCol.innerHTML = `
+                               <div class="card mt-4" style="width: 18rem;">
+                                <img src=${element.imageUrl} class="card-img-top img-fluid">
+                                <div class="card-body">
+                                    <h5 class="card-title">${element.name}</h5>
+                                    <p class="card-text">${element.description}</p>
+                                    <p><span class="badge badge-primary">${element.price}€</span></p>
+                                </div>
+                                </div> 
+                            
+        `;
+        productsContainer.appendChild(newCol);
+    });
+
+}
+
 /* OTHER FUNCTIONS END */
 
 /* EVENT LISTENERS */
